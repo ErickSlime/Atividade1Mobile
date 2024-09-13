2f02300098986b980eccf37ac90bdb737e52a092
Slime <ericksantos1305@gmail.com>
2024-09-13 20:58:32 -0300

    post da atividade

---
+++ Atividade1/app/src/main/java/com/example/atividade1/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.atividade1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+
+        // Recebe o valor do contador da activity 2
+        Intent intent = getIntent();
+        int counter = intent.getIntExtra("counter", 1);
+
+        // Define o texto do botão como o valor do contador
+        button.setText(String.valueOf(counter));
+
+        button.setOnClickListener(v -> {
+            Intent intentToSecond = new Intent(MainActivity.this, Activity2.class);
+            intentToSecond.putExtra("counter", counter + 1);
+            startActivity(intentToSecond);
+        });
+    }
+}
